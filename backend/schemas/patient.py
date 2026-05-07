325ea4700ef4208a39b444b3eef344b60e44ba91
tanvirkashyap <tanvi.gm@gmail.com>
2026-05-07 20:15:57 +0530

    Input variables added

--- backend/schemas/patient.py
+++ backend/schemas/patient.py
@@ -1,1 +1,9 @@
-# patient info file or something
+from pydantic import BaseModel
+
+class Patient(BaseModel):
+    patient_id: str  #import uuid stuff to just generate it
+    age: int
+    heart_rate: int
+    spo2: float
+    chest_pain: bool
+    conscious: bool
