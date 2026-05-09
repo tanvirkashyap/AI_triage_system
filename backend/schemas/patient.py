035ec249d5672d46bd7a240102966fd9ed5d51e8
tanvirkashyap <tanvi.gm@gmail.com>
2026-05-09 13:31:28 +0530

    Default values added

--- backend/schemas/patient.py
+++ backend/schemas/patient.py
@@ -1,9 +1,10 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class Patient(BaseModel):
     patient_id: str  #import uuid stuff to just generate it
     age: int
     heart_rate: int
-    spo2: float
-    chest_pain: bool
-    conscious: bool
+    spo2: float 
+    chest_pain: bool = False
+    unconscious: bool = False
+    pain: int = Field(ge = 0, le = 10)
